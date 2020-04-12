424e274ca90b5a272cf0596c12c8ce19e7922aa0
Agustin Vilte <agustin.vilte@mercadolibre.com>
2020-04-11 21:04:28 -0300

    added sherlock, tyrion and watson model to make functional tests

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 var express = require("express");
 var app = express();
-const port = 8081
-app.listen(port, () => {
- console.log("Server running on port " + port);
+var request = require('request');
+var fs = require('fs');
+
+const PORT = 8081
+const IMG_NAME = 'test.jpg'
+const SHERLOCK_URL = 'http://localhost:8085/execute/sherlock'
+const TYRION_URL = 'http://localhost:8089/execute/tyrion'
+const WATSON_URL = 'http://localhost:8088/execute/watson'
+
+app.listen(PORT, () => {
+ console.log("Server running on port " + PORT);
 });
 
-execution_data = {
-	"license_plate": "ABC123",
-	"category": "old_plate",
-	"score": 90.14,
-	"decisive_model": "watson",
-	"executed_models": {
-		"sherlock": 78.86,
-		"watson": 90.14
-	}
-}
-
 app.post("/recognize/license_plate", (req, res, next) => {
- res.json(execution_data);
+	request({
+        url: TYRION_URL,
+        method: 'POST',
+        headers: {
+			'content-disposition': 'attachment; filename=' + IMG_NAME,
+			'content-type' : 'image/jpg'
+        },
+        encoding: null,
+        body: fs.createReadStream(IMG_NAME)
+   	}, (error, response, body) => {
+        if (error) {
+        	res.json({name : error});
+        } else {
+        	res.json(JSON.parse(response.body.toString()));
+        }
+   	});	
 });
